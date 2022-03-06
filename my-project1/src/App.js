33bea1dced6cd4cb440c609a83e84d0584910e95
dheerajmehta <dheeraj_fw11_310@masai.school>
2022-03-06 13:21:20 +0530

    1

---
+++ my-project1/src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import Navbar from './Components/Navbar'
+// import ProdData from './Components/prod_Data';
+import Prod from './Components/prod';
+import Prod1 from './Components/prod1';
+export default function App() {
+  return (
+    <div className="realtive">
+
+<div className="fixed top-0 left-0 right-0">
+<Navbar />
+</div>
+
+
+
+
+
+
+
+<div>
+<Prod />
+<Prod1 />
+</div>
+
+   
+
+
+
+
+
+  
+
+{/* <ProdData /> */}
+
+   
+    </div>
+  
+
+    
+  )
+}
